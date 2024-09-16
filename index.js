10f79f13f8ae2b3d095548865d9a2609b8484997
suman <sumansapkota777@gmail.com>
2024-09-16 20:09:28 +0545

    feat:LoginAPI

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { connectDatabase } = require("./database/database");
 const User = require("./model/userModel");
 require("dotenv").config();
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 connectDatabase(process.env.MONGO_URI);
+
+//register
 
 app.post("/register", async (req, res) => {
   const { email, password, phoneNumber, username } = req.body;
@@ -33,6 +36,40 @@ app.post("/register", async (req, res) => {
     userPassword: bcrypt.hashSync(password, 10),
     userPhoneNumber: phoneNumber,
   });
+});
+
+// login
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "Please provide email and password",
+    });
+  }
+  const userFound = await User.find({ userEmail: email });
+  if (userFound.length === 0) {
+    return res.status(400).json({
+      message: "User with that email is not registered",
+    });
+  }
+  const isMatched = bcrypt.compareSync(password, userFound[0].userPassword);
+
+  if (isMatched) {
+    //generate token
+
+    const token = jwt.sign({ id: userFound[0]._id }, process.env.SECRET_KEY, {
+      expiresIn: "30d",
+    });
+
+    res.status(200).json({
+      message: "User Logged in Successfully",
+      token,
+    });
+  } else {
+    res.status(404).json({
+      message: "Invalid Password",
+    });
+  }
 });
 
 app.listen(3000, () => {
